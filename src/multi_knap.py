a6e73b063008b5c355fe839aaa2c7e84a1184400
Alex Ionescu <alxi.2001@gmail.com>
2022-09-28 22:58:07 +0200

    Implement minimization with exponential penalty

--- src/multi_knap.py
+++ src/multi_knap.py
@@ -5,22 +5,17 @@ from typing import NoReturn
 
 # Algorithm overview:
 
-# Assuming we have K blackouts, we treat the problem as if we have K knapsacks:
-# The first knapsack correspons to the timeframe from time 0 to the beginning of the first blackout,
-# and the rest of the knapsacks are the periods of time in between each blackout.
-# Note that the period _after_ the last blackout is _not_ considered a knapsack.
+# Assuming we have K blackouts, we treat the problem as if we have K+1 knapsacks:
+# One knapsack for the interval before the first blackout, then knapsacks for the intervals between
+# each blackout, and one "infinite" knapsack _after_ the last blackout.
+# For ease of implementation, we assign the last knapsack capacity equal to the sum of all pictures.
 
-# Then, we apply an algorithm for "multiple knapsack problem" on the K knapsacks.
-# This will fill all the knapsacks in the most efficient way.
+# Then, we solve the problem as if it were a "multiple knapsack problem", but with a twist:
+# The knapsacks have an exponentially increasing penalty, represented by a constant C (here C = 10).
+# e.g. first knapsack has penalty C, the second knapsack has penalty C², the third has penalty C³,
+# and so on.
 
-# There may be "leftover" pictures, that remain to be sent _after_ the last blackout.
-# In this case, knowing that all the knapsacks are "filled to the brim", the total time will be:
-# end_of_last_blackout + sum(leftover_pictures)
-
-# If there are no "leftover" pictures, then the formula is adjusted to use the "last full knapsack"
-# instead of the leftover pictures.
-
-# Here's a visual diagram (Inputs are from data/set1.txt):
+# Here's a visual diagram (Inputs are equivalent to those from data/set1.txt):
 
 # Input picture lengths: 2, 2, 3, 4, 1
 # Input blackouts: 3 5, 8 12
@@ -39,7 +34,7 @@ from typing import NoReturn
 # [[3], [2, 1], [2, 4]]
 # 18
 
-# Based on: https://developers.google.com/optimization/bin/multiple_knapsack
+# Reference: https://developers.google.com/optimization/bin/multiple_knapsack
 
 def fail_with(message: str) -> NoReturn:
   print(message)
@@ -48,21 +43,28 @@ def fail_with(message: str) -> NoReturn:
 Blackout = tuple[float, float]
 Input = tuple[list[float], list[Blackout]]
 
+def parse_blackout(line: str) -> Blackout:
+  [start, duration] = map(float, line.split(","))
+  return (start, start + duration)
+
 def parse_input(path: str) -> Input:
   with open(path, "r") as f:
     num_pictures = int(f.readline())
     pictures = [float(f.readline()) for _ in range(num_pictures)]
 
     num_blackouts = int(f.readline())
-    blackouts = [tuple(map(float, f.readline().split(","))) for _ in range(num_blackouts)]
-    blackouts = list(sorted(map(lambda s: (s[0], s[0] + s[1]), blackouts)))
+    blackouts = [parse_blackout(f.readline()) for _ in range(num_blackouts)]
+    blackouts.sort()
 
     print("Blackouts:", blackouts)
     return (pictures, blackouts)
 
 # Turn a list of blackouts into a list of knapsacks.
-def get_knapsacks(blackouts: list[Blackout]) -> list[float]:
-  return [blackouts[0][0]] + [start - end for ((_, end), (start, _)) in pairwise(blackouts)]
+def get_knapsacks(pictures: list[float], blackouts: list[Blackout]) -> list[float]:
+  return \
+    [blackouts[0][0]] \
+    + [start - end for ((_, end), (start, _)) in pairwise(blackouts)] \
+    + [sum(pictures)]
 
 Output = tuple[list[list[float]], float]
 
@@ -74,7 +76,7 @@ def solve(input: Input) -> Output:
     print("Trivial solution")
     return ([pictures], sum(pictures))
 
-  knapsacks = get_knapsacks(blackouts)
+  knapsacks = get_knapsacks(pictures, blackouts)
   num_knapsacks = len(knapsacks)
 
   solver = pywraplp.Solver.CreateSolver("SCIP") or fail_with("SCIP solver unavailable")
@@ -82,20 +84,21 @@ def solve(input: Input) -> Output:
   # x[p][k] is 1 if picture `p` is in knapsack `k`
   x = [[solver.BoolVar(f"x[{p}, {k}]") for k in range(num_knapsacks)] for p in range(num_pictures)]
 
-  # Constraint: Each picture is in at most one knapsack
+  # Constraint: Each picture is in exactly one knapsack
   for p in range(num_pictures):
-    solver.Add(sum(x[p][k] for k in range(num_knapsacks)) <= 1)
+    solver.Add(sum(x[p][k] for k in range(num_knapsacks)) == 1)
 
   # Constraint: The length of the pictures in each knapsack cannot exceed its capacity
   for k in range(num_knapsacks):
     solver.Add(sum(x[p][k] * pictures[p] for p in range(num_pictures)) <= knapsacks[k])
 
   # Objective: Maximize the length of pictures in each knapsack
+  c = 10
   obj = solver.Objective()
   for p in range(num_pictures):
     for k in range(num_knapsacks):
-      obj.SetCoefficient(x[p][k], pictures[p] * (num_knapsacks - k))
-  obj.SetMaximization()
+      obj.SetCoefficient(x[p][k], pictures[p] * c ** k)
+  obj.SetMinimization()
 
   status = solver.Solve()
   if status != pywraplp.Solver.OPTIMAL:
@@ -103,16 +106,12 @@ def solve(input: Input) -> Output:
 
   # Compute list of pictures, grouped by their knapsack
   knaps = [[pictures[p] for p in range(num_pictures) if x[p][k].solution_value()] for k in range(num_knapsacks)]
-  leftover = [pictures[p] for p in range(num_pictures) if not any(x[p][k].solution_value() for k in range(num_knapsacks))]
 
   # Total time required to send all pictures
-  if leftover:
-    total_time = blackouts[-1][1] + sum(leftover)
-  else:
-    last_full_knapsack = max(k for k in range(num_knapsacks) if knaps[k])
-    total_time = blackouts[last_full_knapsack - 1][1] + sum(knaps[last_full_knapsack])
+  last_full_knapsack = max(k for k in range(num_knapsacks) if knaps[k])
+  total_time = blackouts[last_full_knapsack - 1][1] + sum(knaps[last_full_knapsack])
 
-  return (knaps + [leftover], total_time)
+  return (knaps, total_time)
 
 def main() -> None:
   input_path = argv[1]
