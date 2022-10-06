f10afbdae85c424eedac8f9dd1b6dadfdc34e24d
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2022-10-06 14:33:59 +0200

    added whole directory

--- src/feasibility_check.py
+++ src/feasibility_check.py
@@ -1,4 +1,5 @@
 
+from pathlib import Path
 from intervaltree import Interval, IntervalTree
 
 def parse_blackout(line: str):
@@ -24,15 +25,28 @@ def parse_input(path: str) -> tuple[list[float], list[tuple[float, float]], floa
 
         return pictures, blackouts, expected_total_cost, positions
 
-pictures, blackouts, expected_total_cost, positions = parse_input("assignment/bigger.txt")
+from os import listdir
+def check(path):
+    pictures, blackouts, expected_total_cost, positions = parse_input(path)
 
-assert len(pictures) == len(positions)
-tree = IntervalTree([Interval(a, b) for a, b in blackouts])
+    assert len(pictures) == len(positions)
+    tree = IntervalTree([Interval(a, b) for a, b in blackouts])
 
-for pos, size in zip(positions, pictures):
-    assert pos >= 0
-    assert sorted(tree[pos:pos+size]) == []
-    tree.add(Interval(pos, pos+size))
+    m = -1
+    for pos, size in zip(positions, pictures):
+        assert pos >= 0
+        assert sorted(tree[pos:pos+size]) == []
+        tree.add(Interval(pos, pos+size))
+        if pos+size > m:
+            m = pos+size
+    assert expected_total_cost == m
+
+for d in listdir("./assignment"):
+    print("Checking:", d)
+    check(Path("./assignment") / d)
+    print("--------OK------")
+
+
 
 
 
